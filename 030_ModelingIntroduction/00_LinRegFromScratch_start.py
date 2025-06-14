73d131d54af47004c8dd3c5fbb66e876d6cc6b4a
Botao Deng <dengbotaoo@gmail.com>
2025-06-14 22:56:33 +0800

    linear regression from scratch

--- 030_ModelingIntroduction/00_LinRegFromScratch_start.py
+++ 030_ModelingIntroduction/00_LinRegFromScratch_start.py
@@ -15,9 +15,36 @@ sns.scatterplot(x='wt', y='mpg', data=cars)
 sns.regplot(x='wt', y='mpg', data=cars)
 
 #%% convert data to tensor
-
+X_list = cars.wt.values
+X_np = np.array(X_list, dtype=np.float32).reshape(-1, 1)
+y_list = cars.mpg.values.tolist()
+X = torch.from_numpy(X_np)
+y = torch.tensor(y_list)
 
 #%% training
+w = torch.rand(1, requires_grad=True, dtype=torch.float32)
+b = torch.rand(1, requires_grad=True, dtype=torch.float32)
+
+num_epoch=1000
+learning_rate=0.001
+
+for epoch in range(num_epoch):
+    for i in range(len(X)):
+        # forward pass
+        y_pred = X[i]*w + b
+        # loss
+        loss_tensor = torch.pow(y_pred - y[i], 2)
+        # backward pass
+        loss_tensor.backward()
+        # extract losses
+        loss_value = loss_tensor.data[0]
+        # update w and b
+        with torch.no_gard():
+            w -= w.grad * learning_rate
+            b -= b.grad * learning_rate
+            w.grad.zero_() # _ means inplace ops
+            b.grad.zero_()
+    print(loss_value)
 
 #%% check results
 # %%
